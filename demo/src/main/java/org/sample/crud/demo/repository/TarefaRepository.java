b0af635f46a8be367f8e3060a528b89d5b80bbad
luizfcneto <luizfcneto123@gmail.com>
2024-09-02 09:42:52 -0300

    +adicionando consulta das 3 tarefas pendentes

--- demo/src/main/java/org/sample/crud/demo/repository/TarefaRepository.java
+++ demo/src/main/java/org/sample/crud/demo/repository/TarefaRepository.java
@@ -1,8 +1,13 @@
 package org.sample.crud.demo.repository;
 
+import java.util.List;
+
 import org.sample.crud.demo.entity.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 	
+	@Query("SELECT t FROM Tarefa t WHERE t.pessoaAlocada IS NULL ORDER BY t.prazo ASC LIMIT 3")
+	List<Tarefa> buscaTop3TarefasPendentesSemPessoaAlocada();
 }
